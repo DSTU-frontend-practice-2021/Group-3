603512b068a633c4b798d22990da82c9d3e9e7f4
TheDoctorDiscord <73133119+TheDoctorDiscord@users.noreply.github.com>
2021-06-29 19:03:37 +0300

    Java tasks 1.3, 2.1, 2.2

---
+++ 2-i.js
@@ -0,0 +1,5 @@
+function highAndLow(numbers){
+    let numbarr = numbers.split(' ')
+    let answer = Math.max.apply(Math,numbarr).toString() + ' ' + Math.min.apply(Math,numbarr).toString()
+    return answer
+  }
---
+++ 2-ii.js
@@ -0,0 +1,4 @@
+const findSquares = num => {
+    console.log((num+1)/2 + '-' + (num-1)/2)
+    return ((num+1)/2) ** 2 + '-' + ((num-1)/2)**2
+  };
