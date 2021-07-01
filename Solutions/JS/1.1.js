d28c7fa39d1b74c83fe3d0dbdcad488a26e64fce
Евгений <jamescloudeve@gmail.com>
2021-07-01 23:56:49 +0300

    some fixes

--- Solutions/JS/1.1.js
+++ Solutions/JS/1.1.js
@@ -1,4 +1,3 @@
 const quarterOf = (month) => {
-    let qrtr = [1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4]
-    return qrtr[month - 1]
+    return Math.ceil(month / 3)
 }
--- Solutions/JS/1.2.js
+++ Solutions/JS/1.2.js
@@ -2,8 +2,6 @@ var humanYearsCatYearsDogYears = function(humanYears) {
     switch (humanYears) {
         case 1:
             return [1, 15, 15];
-        case 2:
-            return [2, 24, 24];
         default:
             return [humanYears, 24 + ((humanYears - 2) * 4), 24 + ((humanYears - 2) * 5)];
     }
